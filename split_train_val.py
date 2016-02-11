cddda2dd1eb56c31c5b76778547ca04dab1ee60e
Nikolaus <nikolaus.volk@gmail.com>
2016-02-10 22:34:11 -0800

    updated for full split

--- split_train_val.py
+++ split_train_val.py
@@ -3,45 +3,42 @@ import glob
 import math
 from docopt import docopt
 import shutil
+from random import shuffle
 
 if __name__ == '__main__':
 
-	pic_dir = '/Users/nikolaus/Documents/Stanford_Winter/CS231N/project231/Flickr'
+    pic_dir = '/Users/nikolaus/Documents/Stanford_Winter/CS231N/project231/Flickr'
 
-	train_dir = '/Users/nikolaus/Documents/Stanford_Winter/CS231N/project231/torch_set/train'
+    train_dir = '/Users/nikolaus/Documents/Stanford_Winter/CS231N/project231/torch_set/train'
 
-	val_dir = '/Users/nikolaus/Documents/Stanford_Winter/CS231N/project231/torch_set/val'
+    val_dir = '/Users/nikolaus/Documents/Stanford_Winter/CS231N/project231/torch_set/val'
 
-	label_folders = [name for name in os.listdir(pic_dir)\
-	 if os.path.isdir(os.path.join(pic_dir, name))]
+    label_folders = [name for name in os.listdir(pic_dir)\
+        if os.path.isdir(os.path.join(pic_dir, name))]
 
-	#glob.glob(os.path.join(pic_dir,'*.jpg'))
+    print 'no categories',len(label_folders)
+    #print label_folders
+    for idx,label in enumerate(label_folders):
+    	print 'progress ',label,' ',idx,'/',len(label_folders)
+        path = pic_dir+'/'+label
 
-	#print label_folders
-	for label in label_folders:
-
-		path = pic_dir+'/'+label
-
-
-		pic_list = glob.glob(os.path.join(path,'*.jpg'))
-		#print pic_list[:10]
-		#sys.exit(-1)
-		#shutil.copy()
+        pic_list = glob.glob(os.path.join(path,'*.jpg'))
+        shuffle(pic_list)
+        #print pic_list[:20]
+        #sys.exit(-1)
         train_len = int(0.8*len(pic_list))
 
         train_dir_label = train_dir +'/'+label
         val_dir_label = val_dir +'/'+label
 
         if not os.path.exists(train_dir_label):
-    		os.makedirs(train_dir_label)
+            os.makedirs(train_dir_label)
     	if not os.path.exists(val_dir_label):
-    		os.makedirs(val_dir_label)
+            os.makedirs(val_dir_label)
 
         for pic in pic_list[:train_len]:
             file_root, file_ext = os.path.splitext(pic)
             filename = os.path.basename(file_root)
-            #print filename
-            #sys.exit(-1)
 
             shutil.copy(pic,\
                 os.path.join(train_dir_label, filename+file_ext))
@@ -52,4 +49,4 @@ if __name__ == '__main__':
 
             shutil.copy(pic,\
                 os.path.join(val_dir_label, filename+file_ext))
-        sys.exit(-1)
+        #sys.exit(-1)
